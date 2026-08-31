a651a3da514b4a5b5f8cc93e526b753fb6584628
Daniel Harris-Jordan <danieljjh2003@gmail.com>
2026-08-31 16:09:36 -0400

    Exercise 5: primitive types and local variables

--- labs/Week 1 - Java and JVM Foundations/module-01/exercises/starter/Variables.java
+++ labs/Week 1 - Java and JVM Foundations/module-01/exercises/starter/Variables.java
@@ -1,13 +1,18 @@
 public class Variables {
     public static void main(String[] args) {
-        int age = 0;                       // TODO: set to 21
-        long population = 0L;              // TODO: 8_000_000_000L
-        double price = 0.0;                // TODO: 19.99
-        boolean enrolled = false;          // TODO: true
-        char grade = '?';                   // TODO: 'A'
-        String name = "";                  // TODO: "Aman"
+        int age = 21;                       // TODO: set to 21
+        long population = 8_000_000_000L;              // TODO: 8_000_000_000L
+        double price = 19.99;                // TODO: 19.99
+        boolean enrolled = true;          // TODO: true
+        char grade = 'A';                   // TODO: 'A'
+        String name = "Aman";                  // TODO: "Aman"
 
         // TODO: print each variable on its own line (six println calls)
-        throw new UnsupportedOperationException("TODO: print all six");
+        System.out.println(age);
+        System.out.println(population);
+        System.out.println(price);
+        System.out.println(enrolled);
+        System.out.println(grade);
+        System.out.println(name);
     }
 }
