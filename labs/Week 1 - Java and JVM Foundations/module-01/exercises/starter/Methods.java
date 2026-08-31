83fff2a05fc4313998a0aff2dcf546fc991eaa34
Daniel Harris-Jordan <danieljjh2003@gmail.com>
2026-08-31 16:19:08 -0400

    Exercise 6: methods and stack frames

--- labs/Week 1 - Java and JVM Foundations/module-01/exercises/starter/Methods.java
+++ labs/Week 1 - Java and JVM Foundations/module-01/exercises/starter/Methods.java
@@ -1,17 +1,20 @@
 public class Methods {
     public static void main(String[] args) {
         // TODO: int sum = add(10, 20); then print sum
+        int sum = add(10, 20);
+        System.out.println(sum);
         // TODO: String message = greet("Aman"); then print message
-        throw new UnsupportedOperationException("TODO: call add and greet");
+        String message = greet("Aman");
+        System.out.println(message);
     }
 
     public static int add(int a, int b) {
         // TODO: return a + b
-        throw new UnsupportedOperationException("TODO");
+        return a + b;
     }
 
     public static String greet(String name) {
         // TODO: return "Hello, " + name + "!"
-        throw new UnsupportedOperationException("TODO");
+        return "Hello, " + name + "!";
     }
 }
